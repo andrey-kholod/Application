a940c5a2f52ca865d425bb697790232ea791026e
Andrey-Holod <andrejholod77@gmail.com>
2023-11-06 22:05:54 +0300

    'first'

---
+++ constants/catalog.js
@@ -0,0 +1,18 @@
+const CATALOG = [
+   {
+      id: 'el1',
+      name: 'Ibanez GRG140-WH',
+      img: 'img/catalog/ibanez.webp',
+      price: 29600,
+   }, {
+      id: 'el2',
+      name: '',
+      img: '',
+      price: 0,
+   }, {
+      id: 'el3',
+      name: '',
+      img: '',
+      price: 0,
+   }
+];
