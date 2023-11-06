23417d60f1b2be63e38b892cb79cc9291ac0eff7
Andrey-Holod <andrejholod77@gmail.com>
2023-11-06 22:34:25 +0300

    second

--- constants/catalog.js
+++ constants/catalog.js
@@ -2,17 +2,52 @@ const CATALOG = [
    {
       id: 'el1',
       name: 'Ibanez GRG140-WH',
-      img: 'img/catalog/ibanez.webp',
+      img: 'https://i.ibb.co/QvLQHQr/ibanez.webp"',
       price: 29600,
    }, {
       id: 'el2',
-      name: '',
-      img: '',
-      price: 0,
+      name: 'Ibanez GRG120QASP-BGD',
+      img: 'https://i.ibb.co/YTtRg73/36m4q1mfgm9tntynotg5rrqhu3m1rd7s-jpg.webp" alt="36m4q1mfgm9tntynotg5rrqhu3m1rd7s-jpg',
+      price: 34000,
    }, {
       id: 'el3',
-      name: '',
-      img: '',
-      price: 0,
+      name: 'Ibanez GRG121SP-BMC',
+      img: 'https://i.ibb.co/H422t2r/alkgvezytfgctlhp4slonys76cftdv6b-jpg.webp',
+      price: 36000,
+   }, {
+      id: 'el4',
+      name: 'Ibanez GRX70QA-TRB',
+      img: 'https://i.ibb.co/54hfjJB/x3zpjsc6qq7c80w09l0s399ynaepgmnv-jpg.webp',
+      price: 25800,
+   }, {
+      id: 'el5',
+      name: 'Ibanez GRX70QA-TRB',
+      img: 'https://i.ibb.co/F4bHn7h/a4pkba0zkfa55p8s5tm9ij7m25kb2v1l-jpeg.webp',
+      price: 23400,
+   }, {
+      id: 'el6',
+      name: 'Ibanez GRX40-BKN',
+      img: 'https://i.ibb.co/CKQd3mX/94qinsr4nsyqayrqzz6mki9ilbvk7hro-jpg.webp',
+      price: 23500,
+   }, {
+      id: 'el7',
+      name: 'Ibanez GRX40-BKN',
+      img: 'https://i.ibb.co/YpHdLNW/wz3iemevpvwtl0j0e85zm0e8gj78nwau-jpg.webp',
+      price: 16000,
+   }, {
+      id: 'el8',
+      name: 'Ibanez GRG121DX-BKF',
+      img: 'https://i.ibb.co/NsCdQxC/81ojj8byjuhui1ln9ege5hr5lhgl3awq-jpg.webp',
+      price: 26800,
+   }, {
+      id: 'el9',
+      name: 'EART GW2 Natural',
+      img: 'https://i.ibb.co/xMLKt5Q/hhazdof2eies32p461fi3jpd9c6xc39p-jpg.webp',
+      price: 49980,
+   }, {
+      id: 'el10',
+      name: 'Foix FEG/FEG-KG-30-BK',
+      img: 'https://i.ibb.co/yRVQVdm/wqraev4ycotb4s247v1d117whikqtkb7-jpg.webp',
+      price: 27750,
    }
-];
+]; //каталог
